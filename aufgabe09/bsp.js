da0654059ca70530669546445cbab907c0bde896
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2020-06-28 14:01:41 +0200

    zurück

--- aufgabe09/bsp.js
+++ aufgabe09/bsp.js
@@ -1,37 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.A09Server = void 0;
 const Http = require("http");
 const Url = require("url");
-var Aufgabe09;
-(function (Aufgabe09) {
-    console.log("Starting server");
+var A09Server;
+(function (A09Server) {
+    console.log("Starting server"); // in Konsole wird "starting server" ausgegeben
     let port = Number(process.env.PORT);
+    // wenn der port keinen Wert hat wird im number 8100 zugewiesen.
     if (!port)
         port = 8100;
+    // Instanz "server" wird erstellt, dann wird ein Server erstellt
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
+    //Server soll port abhören
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); // in Konsole wird "Listening" ausgegeben wenn der addListener ausgeführt wird
     }
     function handleRequest(_request, _response) {
-        console.log("I hear voices!");
+        console.log("I hear voices!"); // in Konsole wird "i hear voices" ausgegeben wenn der addListener ausgeführt wird
+        //response parameter
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         if (_request.url) {
             let url = Url.parse(_request.url, true);
             if (url.pathname == "/html") {
                 for (let key in url.query) {
-                    _response.write(key + ": " + url.query[key] + "<br>");
+                    _response.write(key + ":" + url.query[key] + "<br/>");
                 }
             }
             if (url.pathname == "/json") {
                 let jsonString = JSON.stringify(url.query);
                 _response.write(jsonString);
+                console.log(jsonString);
             }
-            _response.end();
         }
+        _response.end();
     }
-})(Aufgabe09 || (Aufgabe09 = {}));
+})(A09Server = exports.A09Server || (exports.A09Server = {}));
 //# sourceMappingURL=bsp.js.map
--- aufgabe09/bsp.ts
+++ aufgabe09/bsp.ts
@@ -1,49 +1,48 @@
 import * as Http from "http";
 import * as Url from "url";
 
-namespace Aufgabe09 {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
+export namespace A09Server {
 
-    
-    if (!port)
-        port = 8100;
+  console.log("Starting server"); // in Konsole wird "starting server" ausgegeben
+  let port: number = Number(process.env.PORT);
 
-    
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+  // wenn der port keinen Wert hat wird im number 8100 zugewiesen.
+  if (!port)
+    port = 8100;
 
-    
-    function handleListen(): void {
-        console.log("Listening");
-    }
+  // Instanz "server" wird erstellt, dann wird ein Server erstellt
+  let server: Http.Server = Http.createServer();
+  server.addListener("request", handleRequest);
+  server.addListener("listening", handleListen);
+  server.listen(port);
+  //Server soll port abhören
 
-    
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
+  function handleListen(): void {
+    console.log("Listening"); // in Konsole wird "Listening" ausgegeben wenn der addListener ausgeführt wird
+  }
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+  function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    console.log("I hear voices!");  // in Konsole wird "i hear voices" ausgegeben wenn der addListener ausgeführt wird
 
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+    //response parameter
 
-        if (_request.url) {
-            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("Access-Control-Allow-Origin", "*");
 
-            if (url.pathname == "/html") {
-                for (let key in url.query) {
-                    _response.write(key + ": " + url.query[key] + "<br>");
-                }
-            }
-
-            if (url.pathname == "/json") {
-                let jsonString: string = JSON.stringify(url.query);
-                _response.write(jsonString);
-            }
-
-            _response.end();
-            
+    if (_request.url) {
+      let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+      if (url.pathname == "/html") {
+        for (let key in url.query) {
+          _response.write(key + ":" + url.query[key] + "<br/>");
         }
+      }
+      if (url.pathname == "/json") {
+      let jsonString: string = JSON.stringify(url.query);
+
+      _response.write(jsonString);
+      console.log(jsonString);
+       }
     }
+    _response.end();
+  }
 }
