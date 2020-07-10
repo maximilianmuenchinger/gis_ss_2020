0449fad8b6ec924f139a2673c670640ea2d8e1f5
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2020-07-10 12:19:52 +0200

    aufgabe11 test

---
+++ aufgabe11/server.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A11Server = void 0;
+const Http = require("http");
+const Url = require("url");
+const Mongo = require("mongodb");
+var A11Server;
+(function (A11Server) {
+    let orders;
+    let port = Number(process.env.PORT);
+    // wenn der port keinen Wert hat wird im number 8100 zugewiesen.
+    if (!port)
+        port = 8100;
+    let databaseUrl = "mongodb+srv://User1:<password>@maxscluster.juvc9.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    startServer(port);
+    connectToDatabse(databaseUrl);
+    function startServer(_port) {
+        console.log("Starting server"); // in Konsole wird "starting server" ausgegeben
+        // Instanz "server" wird erstellt, dann wird ein Server erstellt
+        let server = Http.createServer();
+        server.addListener("request", handleRequest);
+        server.addListener("listening", handleListen);
+        server.listen(_port);
+        //Server soll port abhören
+    }
+    async function connectToDatabse(_url) {
+        let options = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        orders = mongoClient.db("Test").collection("Students");
+        console.log("Database connection", orders != undefined);
+    }
+    function handleListen() {
+        console.log("Listening"); // in Konsole wird "Listening" ausgegeben wenn der addListener ausgeführt wird
+    }
+    async function handleRequest(_request, _response) {
+        console.log("I hear voices!"); // in Konsole wird "i hear voices" ausgegeben wenn der addListener ausgeführt wird
+        console.log("test1234");
+        //response parameter
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            if (url.pathname == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ":" + url.query[key] + "<br/>");
+                    orders.insertOne(url.query);
+                }
+            }
+            else {
+                _response.write(JSON.stringify(await orders.find().toArray()));
+            }
+        }
+        _response.end();
+    }
+})(A11Server = exports.A11Server || (exports.A11Server = {}));
+//# sourceMappingURL=server.js.map
---
+++ aufgabe11/server.ts
@@ -0,0 +1,67 @@
+import * as Http from "http";
+import * as Url from "url";
+import * as Mongo from "mongodb";
+
+export namespace A11Server {
+
+    let orders: Mongo.Collection;
+
+    let port: number = Number(process.env.PORT);
+
+    // wenn der port keinen Wert hat wird im number 8100 zugewiesen.
+    if (!port)
+        port = 8100;
+
+
+    let databaseUrl: string = "mongodb+srv://User1:<password>@maxscluster.juvc9.mongodb.net/<dbname>?retryWrites=true&w=majority";
+    startServer(port);
+    connectToDatabse(databaseUrl);
+    function startServer(_port: number | string): void {
+
+        console.log("Starting server"); // in Konsole wird "starting server" ausgegeben
+
+
+        // Instanz "server" wird erstellt, dann wird ein Server erstellt
+        let server: Http.Server = Http.createServer();
+        server.addListener("request", handleRequest);
+        server.addListener("listening", handleListen);
+        server.listen(_port);
+        //Server soll port abhören
+    }
+    async function connectToDatabse(_url: string): Promise<void> {
+        let options: Mongo.MongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+        let mongoClient: Mongo.MongoClient = new Mongo.MongoClient(_url, options);
+        await mongoClient.connect();
+        orders = mongoClient.db("Test").collection("Students");
+        console.log("Database connection", orders != undefined);
+    }
+
+
+
+    function handleListen(): void {
+        console.log("Listening"); // in Konsole wird "Listening" ausgegeben wenn der addListener ausgeführt wird
+    }
+
+    async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
+        console.log("I hear voices!");  // in Konsole wird "i hear voices" ausgegeben wenn der addListener ausgeführt wird
+        console.log("test1234");
+        //response parameter
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            if (url.pathname == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ":" + url.query[key] + "<br/>");
+                    orders.insertOne(url.query);
+                }
+            }
+            else {
+                _response.write(JSON.stringify(await orders.find().toArray()));
+            }
+        }
+        _response.end();
+    }
+}
